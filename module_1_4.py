b52f8e9fabda2dc127f39c2039df92b23023f47d
Grigor1911 <grixan.1941@mail.ru>
2024-08-11 22:05:20 +0500

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input("Моя строка: ")
+print("Чья это строка?", my_string)
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ", ""))
+print(my_string[0])
+print(my_string[13])
